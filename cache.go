9b712177961afe8ba7e6680aba8b90db00df6e71
mdmitry <ru.system@mail.ru>
2022-07-27 19:51:22 +0400

    fixed concurrency errors, add ttl

--- cache.go
+++ cache.go
@@ -1,23 +1,67 @@
 package cache
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
 type Cache struct {
 	info map[string]interface{}
+	mu   *sync.RWMutex
 }
 
 func New() *Cache {
 	return &Cache{
 		make(map[string]interface{}),
+		new(sync.RWMutex),
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.info[key] = value
+func (c *Cache) ttl(key string, ttl time.Duration) {
+	for {
+		select {
+		case <-time.After(ttl):
+			err := c.Delete(key)
+			if err != nil {
+				log.Fatal()
+			}
+			return
+		}
+	}
 }
 
-func (c *Cache) Get(key string) interface{} {
-	return c.info[key]
-}
+func (c *Cache) Delete(key string) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-func (c *Cache) Delete(key string) {
+	if _, ok := c.info[key]; !ok {
+		return errors.New(fmt.Sprintf("Not found value by key=%s", key))
+	}
 	delete(c.info, key)
+
+	return nil
+}
+
+func (c *Cache) Get(key string) (interface{}, error) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
+	_, ok := c.info[key]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("Not found value by key=%s", key))
+	}
+
+	return c.info[key], nil
+}
+
+func (c *Cache) Set(key string, value interface{}, ttl time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.info[key] = value
+
+	go c.ttl(key, ttl)
 }
